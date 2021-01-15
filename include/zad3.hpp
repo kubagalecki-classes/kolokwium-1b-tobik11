da7f5c9d4eefdae2ec8279ac74066cea8e92dc04
tobik11 <tobik11@onet.pl>
2021-01-15 08:00:50 +0000

    zad3v1

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -3,3 +3,18 @@
 #include "zad1.hpp"
 
 //tutaj szablon ArtystaPlusInstrument
+template<typename T>
+class ArtystaPlusInstrument {
+public:
+	ArtystaPlusInstrument(const Artysta& _a, const T& _i) {
+		a = _a;
+		i = _i;
+	}
+
+	unsigned koncert() {
+		returned i.liczbaUczestnikow(a.gamers);
+	}
+private:
+	Artysta a;
+	T i;
+};
