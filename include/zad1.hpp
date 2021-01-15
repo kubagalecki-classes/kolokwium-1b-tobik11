284e81f7a94efe5cb5928fd5237449c552ab6bd7
tobik11 <tobik11@onet.pl>
2021-01-15 07:25:39 +0000

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,26 @@
 #include <string>
 
 // tutaj klasa Artysta
+
+class Artysta {
+public:
+
+	Artysta() : pseudonim("Anonim") {}
+	Artysta(const std::string& _pseu, unsigned _gam) : pseudonim(_pseu), gamers(_gam) {}
+
+	void graj(std::ostream& kk) const {
+		kk << "[" << pseudonim << "]:[" << gamers << "]" << std::endl;
+	}
+
+	const std::string& getPseudonim() const {
+		return pseudonim;
+	}
+
+	unsigned getSluchacze() const {
+		return gamers;
+	}
+
+private:
+	unsigned gamers = 0;
+	std::string pseudonim;
+};
