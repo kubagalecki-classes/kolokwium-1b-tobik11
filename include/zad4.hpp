7d39b96f09a658c6a7dbda6e4a430990c07f994c
tobik11 <tobik11@onet.pl>
2021-01-15 08:14:46 +0000

    zad4v1

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -4,3 +4,13 @@
 #include <vector>
 
 // tutaj funkcja stworzZespol
+std::vector<Artysta> stworzZespol(int a, const std::string& st) {
+	if (st.size() != a) {
+		std::logic_error("error");
+	}
+	else {
+		std::vector<Artysta> tren;
+		for (int i = 0; i < a; i++)
+			tren.push_back(Artysta(st, i + 1));
+	}
+}
