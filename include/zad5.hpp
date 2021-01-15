5b8a5c00a821ede2f7d687fbbe5273fc4a9af96f
tobik11 <tobik11@onet.pl>
2021-01-15 08:31:10 +0000

    zad5v1

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -5,3 +5,8 @@
 #include <vector>
 
 // tutaj algorytm liczPopularnych(T begin, T end)
+
+template<typename T>
+void liczPopularnych(T a, T b) {
+	int num = count_if(a, b, [](unsigned i.gamers) {return i>1000})
+}
